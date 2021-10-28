4e155e2e367784c18a8f06ec7bf103fd57ad67a0
Lolongzm <92027784+Lolongzm@users.noreply.github.com>
2021-10-28 20:23:29 +0800

    Add files via upload

---
+++ test_10_28/test_10_28/2021-10-28 test.c
@@ -0,0 +1,92 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+
+//int main()
+//{
+//	//printf("%s\n", __FILE__);
+//	//printf("%d\n", __LINE__);
+//	//printf("%s\n", __DATE__);
+//	//printf("%s\n", __TIME__);
+//
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	FILE* pf = fopen("log.txt", "w");
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//		fprintf(pf, "file:%s line:%d time:%s data:%s i=%d\n",
+//			__FILE__, __LINE__, __TIME__, __DATE__,i);
+//	}
+//	fclose(pf);
+//	pf = NULL;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//
+//#define MAX 100
+//#define reg register
+//int mian()
+//{
+//	int max = MAX;
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+//#define SQUARE(X) X*X
+//
+//int main()
+//{
+//	int ret = SQUARE(5+1);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#define DOUBLE(X) ((X)+(X))
+//
+//int main()
+//{
+//	int a = 5;
+//	int ret = 10 * DOUBlE(a);
+//	return 0;
+//}
+
+//
+//#define PRINT(X) printf("the value of " #X " is %d\n",X)
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	PRINT(a);
+//	//printf("the value of" "a" "is %d\n", a);
+//
+//	PRINT(b);
+//	//printf("the value of" "b" "is %d\n", b);
+//
+//	return 0;
+//}
+
+//#define CAT(X,Y) X##Y
+//
+//int main()
+//{
+//	
+//	int Class84 = 2019;
+//	int Class85 = 2020;
+//	while (1)
+//	{
+//		int i = 84;
+//		printf("%d\n", CAT(Class, i));
+//		i++;
+//		//printf("%d\n", Class##84);
+//	}
+//
+//	return 0;
+//}
+
+
