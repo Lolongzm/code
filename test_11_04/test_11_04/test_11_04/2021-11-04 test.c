5cdfb3d3265083d5b9d83ae4ad42c7b60641aab0
Lolongzm <92027784+Lolongzm@users.noreply.github.com>
2021-11-08 21:03:25 +0800

    Add files via upload

---
+++ test_11_04/test_11_04/test_11_04/2021-11-04 test.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"SqList.h"
+
+int main()
+{
+	struct SqList L;
+
+	InitList(&L);
+	ClearList(&L);
+	DestroyList(&L);
+
+	int i = 0;
+	type* e = (type*)malloc(sizeof(type));
+	GetElem(L, i, &e);
+
+	int x = 0;
+	printf("请输入插入的位置（第几个）:>\n");
+	scanf("%d", &x);
+	type e = { 0,0,0 };
+	printf("请输入插入图书的书名,编号，价格:>\n");
+	scanf("%s %d %d", &(e.name),&(e.number),&(e.price));
+	Insert(L, x, e);
+	int i = 0;
+	for (i = 0; i < L.length; i++)
+	{
+		printf("name=%s number=%d price=%d\n", L.data[i].name, L.data[i].number, L.data[i].price);
+	}
+
+
+
+	return 0;
+}
